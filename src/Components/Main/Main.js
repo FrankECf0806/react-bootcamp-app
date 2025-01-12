b231548dbbdc50e162364e67e2d247fa39859edf
Frank Cruz <fcruz@ctdevelopers.com>
2025-01-12 13:51:13 +0000

    [Miniproject Part 2] 	- Worked on the Main Componet view 	- Added styling and 2 items: 		- TextField and Box 		- Button

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import './Main.css'
+import { Box, TextField } from '@mui/material'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div className='main'>
+        <Container>
+            <Row>
+                <Col>
+                    <h1> What is in the calendar today? </h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={9} md={10}>
+                    <Box
+                        className="main-input"
+                        sx={{
+                                maxWidth: '100%'
+                            }}>
+                        <TextField
+                            fullWidth
+                            label="Add your items"
+                            id="fullWidth" />
+                    </Box>
+                </Col>
+                <Col xs={3} md={2} className='main-col-button'>
+                    <Button
+                        className='main-button'
+                        variant="outline-primary">
+                        Add
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    </div>
   )
 }
